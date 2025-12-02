e5369c25fcf1b58fea2d2394c37fa35e09505d51
JDDev0 <jddev0@dorneger.at>
2025-12-02 20:32:18 +0100

    Use minimal bevy setup for CLI build

--- src/ui/cli.rs
+++ src/ui/cli.rs
@@ -1,6 +1,11 @@
+use std::error::Error;
 use std::process::ExitCode;
-use std::thread::sleep;
 use std::time::Duration;
+use bevy_app::prelude::*;
+use bevy_app::ScheduleRunnerPlugin;
+use bevy_ecs::prelude::*;
+use bevy_time::prelude::*;
+use bevy_time::TimePlugin;
 use crate::game::Game;
 use crate::io::Console;
 
@@ -13,28 +18,60 @@ pub fn run_game() -> ExitCode {
         return exit_code;
     }
 
-    let console = Console::new().unwrap();
+    let console = Box::leak(Box::new(Console::new().unwrap()));
 
-    let game = Game::new(&console);
-    let mut game = match game {
-        Ok(game) => game,
-        Err(err) => {
-            drop(console);
+    let ret = run_game_internal(console);
 
-            eprintln!("{err}");
+    // Drop of Console must be called to restore the terminal mode
+    //
+    // SAFETY: The mutable reference &mut console is valid before calling "Box::from_raw()"
+    // and is no longer used afterward
+    drop(unsafe { Box::from_raw(console as *const _ as *mut Console) });
 
-            return ExitCode::FAILURE;
-        },
+    ret.unwrap_or_else(|err| {
+        // The terminal mode is restored in the Console's Drop implementation,
+        // therefore it must be dropped before the error output is printed.
+
+        eprintln!("{err}");
+
+        ExitCode::FAILURE
+    })
+}
+
+fn run_game_internal(console: &'static Console) -> Result<ExitCode, Box<dyn Error>> {
+    let game = Game::new(console)?;
+
+    let mut app = App::new();
+
+    app.
+            add_plugins(TaskPoolPlugin::default()).
+            add_plugins(TimePlugin).
+            add_plugins(ScheduleRunnerPlugin::run_loop(Duration::from_millis(40))).
+
+            insert_resource(Time::<Fixed>::from_seconds(0.040)). //Run FixedUpdate every 40ms
+
+            insert_non_send_resource(game).
+
+            add_systems(FixedUpdate, update_game);
+
+    let exit_code = app.run();
+    let exit_code = match exit_code {
+        AppExit::Success => ExitCode::SUCCESS,
+        AppExit::Error(code) => ExitCode::from(code.get()),
     };
 
-    loop {
-        let should_stop = game.update();
-        game.draw();
+    Ok(exit_code)
+}
 
-        if should_stop {
-            return ExitCode::SUCCESS;
-        }
+fn update_game(
+    mut game: NonSendMut<Game>,
 
-        sleep(Duration::from_millis(40));
+    mut app_exit_event_writer: MessageWriter<AppExit>,
+) {
+    let should_stop = game.update();
+    game.draw();
+
+    if should_stop {
+        app_exit_event_writer.write(AppExit::Success);
     }
 }
